33842902353f43e8121b0ddd66515100040fa809
gy <1838940294@qq.com>
2021-12-12 20:44:13 +0800

    feat: Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Button from './components/Button/button';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
+import SubMenu from './components/Menu/subMenu';
 
 function App() {
   return (
     <div className="App">
-      <h1>gaoyang</h1>
-      <code>const a = 'b'</code>
+      <Menu mode={'vertical'}>
+        <SubMenu title={'sgy'}>
+          <MenuItem>shen</MenuItem>
+          <MenuItem>gao</MenuItem>
+          <MenuItem>yang</MenuItem>
+        </SubMenu>
+        <MenuItem>shen</MenuItem>
+        <MenuItem>gao</MenuItem>
+        <MenuItem>yang</MenuItem>
+      </Menu>
       <Button>按钮</Button>
       <Button btnType={'primary'} href={'www.sgyat.cn'} size={'sm'}>
         按钮
