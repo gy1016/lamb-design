bd26a89570dd10e3996fc11c749afbd2bc7aec08
gy <1838940294@qq.com>
2021-12-12 17:38:36 +0800

    feat: Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>gaoyang</h1>
+      <code>const a = 'b'</code>
+      <Button>按钮</Button>
+      <Button btnType={'primary'} href={'www.sgyat.cn'} size={'sm'}>
+        按钮
+      </Button>
     </div>
   );
 }
